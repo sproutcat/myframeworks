b49e772e9717ff4507c69ab6c5c2de9d030b49c5
tangguo <tang_guo_168@163.com>
2013-07-28 00:17:44 +0800

    修改基础控制器

--- WebRoot/js/app/baseui/BaseController.js
+++ WebRoot/js/app/baseui/BaseController.js
@@ -3,21 +3,35 @@
 
 
 
-Ext.define('App.controller.sys.AuthModule', {
+Ext.define('App.baseui.BaseController', {
 	extend : 'Ext.app.Controller',
 	init: function(application) {
-		
+		var allControl = {
+			"baseGrid": {
+				itemclick: function(grid, record, item, index, e, eOpts ) {
+					console.log(record);
+				}
+			}
+		};
+		Ext.apply(allControl, this.getMyContral(application));
+		this.control(allControl);
 	},
 	
 	/**
 	 * @Override
 	 */
-	loadModel : function() {
-		
-	},
+	loadModel : function() {},
 
 	/**
 	 * @Override
 	 */
-	loadView : function() {}
+	loadView : function() {},
+	
+	/**
+	 * 自定义控制
+	 * @return {}
+	 */
+	getMyContral: function(application) {
+		return {};
+	}
 });
